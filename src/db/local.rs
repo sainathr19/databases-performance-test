3c036f51d5b6a6382ef082ee71602d02d0c6818a
Sainath Reddy <gsnr1925@gmail.com>
2024-12-12 01:15:10 +0530

    fix : modify w.r.t change in Database

--- src/db/local.rs
+++ src/db/local.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use crate::models::RpmuHistoryInterval;
 use crate::db::{Database, DatabaseError};
+use crate::helpers::timer::Timer;
 
 #[derive(Default)]
 pub struct InMemoryDatabase {
@@ -14,51 +15,56 @@ impl Database for InMemoryDatabase {
     async fn init() -> Result<Self, DatabaseError> {
         let db = InMemoryDatabase::default();
         let dummy_record = RpmuHistoryInterval {
-            start_time: 1733077078 as f64,
-            count : 1733077078 as f64,
-            end_time : 1234 as f64,
-            units : 4 as f64
+            start_time: 1669918678.0,
+            count: 1669918678.0,
+            end_time: 1234.0,
+            units: 4.0,
         };
         db.insert_one(&dummy_record).await?;
         Ok(db)
     }
 
     async fn insert_one(&self, data: &RpmuHistoryInterval) -> Result<u64, DatabaseError> {
+        let mut timer = Timer::init();
+        timer.start();
+
         let mut map = self.data.lock().unwrap();
         let id = data.start_time as u64;
         map.insert(id, data.clone());
-        Ok(id)
+
+        let elapsed_time = timer.stop();
+        Ok(elapsed_time as u64)
     }
 
     async fn insert_many(&self, data: Vec<RpmuHistoryInterval>) -> Result<u64, DatabaseError> {
+        let mut timer = Timer::init();
+        timer.start();
+
         let mut map = self.data.lock().unwrap();
         for item in data.clone() {
             let id = item.start_time as u64;
             map.insert(id, item);
         }
-        Ok(data.len() as u64)
+
+        let elapsed_time = timer.stop();
+        Ok(elapsed_time as u64)
     }
 
-    async fn fetch_one(&self, filter: Option<serde_json::Value>) -> Result<RpmuHistoryInterval, DatabaseError> {
-        let map = self.data.lock().unwrap();
-        if let Some(filter_value) = filter {
-            let start_time = filter_value.as_u64().ok_or(DatabaseError::UnknownError)?;
-            if let Some(interval) = map.get(&start_time) {
-                return Ok(interval.clone());
-            }
-        }
-        Err(DatabaseError::UnknownError)
-    }
+    async fn fetch_all(&self) -> Result<(u64,Vec<RpmuHistoryInterval>), DatabaseError> {
+        let mut timer = Timer::init();
+        timer.start();
 
-    async fn fetch_all(&self, _filter: Option<serde_json::Value>) -> Result<Vec<RpmuHistoryInterval>, DatabaseError> {
         let map = self.data.lock().unwrap();
         let intervals: Vec<RpmuHistoryInterval> = map.values().cloned().collect();
-        Ok(intervals)
+
+        let elapsed_time = timer.stop();
+        println!("Fetch all completed in {:.2} seconds.", elapsed_time);
+        Ok((elapsed_time as u64,intervals))
     }
 
     async fn fetch_latest_timestamp(&self) -> Result<u64, DatabaseError> {
         let map = self.data.lock().unwrap();
-        if let Some((latest_start_time, _)) = map.iter().max_by_key(|(&end_time, _)| end_time) {
+        if let Some((latest_start_time, _)) = map.iter().max_by_key(|(&start_time, _)| start_time) {
             return Ok(*latest_start_time);
         }
         Err(DatabaseError::UnknownError)
